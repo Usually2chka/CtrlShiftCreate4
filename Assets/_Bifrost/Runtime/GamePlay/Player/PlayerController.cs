ea2ca70d4cea36bd27fd251e877ff81cec9b87c6
Usually2chka <127783284+Usually2chka@users.noreply.github.com>
2026-04-18 03:24:41 +0300

    add jump

--- Assets/_Bifrost/Runtime/GamePlay/Player/PlayerController.cs
+++ Assets/_Bifrost/Runtime/GamePlay/Player/PlayerController.cs
@@ -9,7 +9,12 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _speed = 5f;
     [SerializeField] private float _mouseSensitivity = 25f;
     [SerializeField] private Transform _playerCamera;
-
+    [SerializeField] private Rigidbody _rigidbody;
+    [SerializeField] private float _jumpForce = 1.5f;
+    [SerializeField] private float _gravity = -9.81f;
+    
+    private float _yVelocity;
+    
     private CharacterController _controller;
     private Vector2 _moveInput;
     private Vector2 _lookInput;
@@ -18,8 +23,6 @@ public class PlayerController : MonoBehaviour
     private InputSystem_Actions _inputActions;
     
     private InteractiveObject _current;
-    private InteractiveObject _previousItem;
-    private LayerMask _interactLayer;
 
     private void Awake()
     {
@@ -30,8 +33,10 @@ public class PlayerController : MonoBehaviour
         _inputActions.Player.Look.performed += ctx => _lookInput = ctx.ReadValue<Vector2>();
 
         _inputActions.Player.Look.canceled += ctx => _lookInput = Vector2.zero;
+        
+        _inputActions.Player.Jump.performed += ctx => Jump();
     }
-
+    
     private void Start()
     {
         _controller = GetComponent<CharacterController>();
@@ -56,7 +61,17 @@ public class PlayerController : MonoBehaviour
     
     private void HandleMovement()
     {
+        if (_controller.isGrounded && _yVelocity < 0)
+        {
+            _yVelocity = -2f; // "прилипание" к земле
+        }
+
         Vector3 move = transform.right * _moveInput.x + transform.forward * _moveInput.y;
+
+        _yVelocity += _gravity * Time.deltaTime;
+
+        move.y = _yVelocity;
+
         _controller.Move(move * _speed * Time.deltaTime);
     }
 
@@ -71,7 +86,15 @@ public class PlayerController : MonoBehaviour
         _playerCamera.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
     }
-
+    
+    private void Jump()
+    {
+        if (_controller.isGrounded)
+        {
+            _yVelocity = Mathf.Sqrt(_jumpForce * -2f * _gravity);
+        }
+    }
+    
     private void Interact()
     {
         Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
