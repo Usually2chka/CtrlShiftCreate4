0ca383a7587f4def23b71ae68315364ecf5bb529
Usually2chka <127783284+Usually2chka@users.noreply.github.com>
2026-04-19 16:24:44 +0300

    VFX fog and fire

---
+++ Assets/VFXTextureMaker/Editor/ShaderPass.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace VFXTextureMaker
+{
+    [Serializable]
+    public enum ShaderPass
+    {
+        Clear,
+        Draw_Texture,
+        Displacement_Texture,
+        Displacement_NormalMap,
+        Draw_Noise,
+        Draw_Shape,
+        Displacement_Noise,
+        Filter_Blur,
+        Filter_GradationSample,
+        Filter_Glow,
+        Filter_ColorBalance,
+        CustomShader,
+        Draw_Draw,
+        Displacement_Displacement,
+        Draw_TileAndOffset,
+        Filter_Posterize
+    }
+}
